043c5b2e4a2b1a5baa2d85790f0a9cc4a553637a
Ns2Kracy <ns2kracy@gmail.com>
2024-10-23 13:26:07 +0800

    chore: update package versions and remove unused route component

--- src/routes/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div class="flex min-h-screen flex-col justify-center gap-6 text-center dark:bg-neutral-800 dark:text-neutral-200">
-      <h1 class="text-center text-4xl font-semibold">Maestro</h1>
-    </div>
-  );
-}
